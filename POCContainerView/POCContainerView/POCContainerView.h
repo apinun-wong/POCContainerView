dc18b406efed81b4e240a75db8fe9fab18ee3815
Apinun <apinun.wong@gmail.com>
2019-06-23 00:07:21 +0700

    first commit

---
+++ POCContainerView/POCContainerView/POCContainerView.h
@@ -0,0 +1,19 @@
+//
+//  POCContainerView.h
+//  POCContainerView
+//
+//  Created by Apinun Wongintawang on 6/23/19.
+//  Copyright © 2019 Apinun Wongintawang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for POCContainerView.
+FOUNDATION_EXPORT double POCContainerViewVersionNumber;
+
+//! Project version string for POCContainerView.
+FOUNDATION_EXPORT const unsigned char POCContainerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <POCContainerView/PublicHeader.h>
+
+
